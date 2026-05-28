bae076acf4121899f870048f674fc0ab4e9b44ae
kousikcreativestudio <kousikcreativedesigns@gmail.com>
2026-05-28 11:24:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,60 @@
 function openSurprise(){
+
     document.getElementById("surpriseBox").style.display="block";
+
     document.getElementById("bgMusic").play();
-    startVideoSlideshow();
-    typingText();
+
+    startSlideshow();
+    createBalloons();
+    typing();
 }
 
-let index = 0;
+// 🎬 slideshow
+let i = 0;
 let slides = document.querySelectorAll(".vslide");
 
-function startVideoSlideshow(){
+function startSlideshow(){
     setInterval(()=>{
-        slides[index].classList.remove("active");
-        index = (index + 1) % slides.length;
-        slides[index].classList.add("active");
+        slides[i].classList.remove("active");
+        i = (i+1)%slides.length;
+        slides[i].classList.add("active");
     },3000);
 }
 
-function typingText(){
-    let text = "Happy Birthday Sahitya 💖 You are my world!";
-    let i = 0;
-    function type(){
-        if(i < text.length){
-            document.getElementById("surpriseText").innerHTML += text.charAt(i);
+// 🎂 cake cutting
+function cutCake(){
+
+    document.getElementById("cake").style.transform="scale(0.8) rotate(-10deg)";
+
+    document.getElementById("cutSound").play();
+
+    alert("🎉 Cake Cut! Happy Birthday 💖");
+}
+
+// 🎈 balloons
+function createBalloons(){
+
+    for(let i=0;i<15;i++){
+        let b=document.createElement("img");
+        b.src="https://i.imgur.com/7QZ7Z6F.png";
+        b.className="balloon";
+        b.style.left=Math.random()*100+"%";
+        document.body.appendChild(b);
+    }
+}
+
+// 💬 typing
+function typing(){
+
+    let text="Happy Birthday Sahitya 💖";
+    let i=0;
+
+    function t(){
+        if(i<text.length){
+            document.getElementById("surpriseText").innerHTML+=text[i];
             i++;
-            setTimeout(type,60);
+            setTimeout(t,50);
         }
     }
-    type();
+    t();
 }
